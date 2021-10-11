1d2e8b13308a546fd4e25381df8cfb1c7cfd4a52
Fachrul Rivaldy <fachrul.rivaldy13@gmail.com>
2021-10-11 11:31:33 +0700

    setup firestore

--- lib/mainpage/main.dart
+++ lib/mainpage/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:dtk_database_tekkom/mainpage/loginpage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MaterialApp(
     home: LoginPage(),
   ));
